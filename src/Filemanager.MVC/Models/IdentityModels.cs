d778636b1a740df5cc5d4ab829735548ab0bb8a3
Intoccabil <i.ulyanoff@gmail.com>
2014-05-07 11:54:54 +0300

    Initial commit.

---
+++ src/Filemanager.MVC/Models/IdentityModels.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.Entity;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Filemanager.MVC.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        static ApplicationDbContext()
+        {
+            Database.SetInitializer(new MyDbInitializer());
+        }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+
+    public class MyDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            const string AdminRole = "Admin";
+
+            const string AdminUsername = "Administrator";
+            const string AdminPassword = "filemanager";
+
+            if (!roleManager.RoleExists(AdminRole))
+            {
+                roleManager.Create(new IdentityRole(AdminRole));
+            }
+
+            ApplicationUser user = userManager.FindByName(AdminUsername);
+
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = AdminUsername };
+                userManager.Create(user, AdminPassword);
+            }
+
+            if (!userManager.IsInRole(user.Id, AdminRole))
+            {
+                userManager.AddToRole(user.Id, AdminRole);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ src/Filemanager.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Filemanager.MVC.Startup))]
+namespace Filemanager.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
